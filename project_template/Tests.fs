a9a5e33c41005e91a975f7070be23522d05a5b75
Spencer Williams <spencerwi@gmail.com>
2024-12-16 14:48:11 -0500

    Tweak template

--- project_template/Tests.fs
+++ project_template/Tests.fs
@@ -1,5 +1,5 @@
 module Tests
-open Lib
+open Lib.Puzzle
 
 open FsUnit
 open NUnit.Framework
@@ -11,13 +11,19 @@ Fill me in from the problem description!
 let sample_input = sample_input_raw.Trim().Split "\n"
 
 [<TestFixture>]
+type ``Parsing Tests`` ()=
+    [<Test>]
+    member _.``It should parse input`` ()=
+        1 |> should equal 1 // TODO
+
+[<TestFixture>]
 type ``Tests for solution`` ()=
     [<Test>]
-    member this.``It should solve part 1`` ()=
-        Puzzle.part1 sample_input
+    member _.``It should solve part 1`` ()=
+        part1 sample_input
         |> should equal "the right answer"
 
     [<Test>]
-    member this.``It should solve part 2`` ()=
-        Puzzle.part2 sample_input
+    member _.``It should solve part 2`` ()=
+        part2 sample_input
         |> should equal "the right answer"
